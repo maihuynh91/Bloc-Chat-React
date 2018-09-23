b43eaceb627d7dc8cc090a6261e0b9292c2faba0
Mai Huynh <nhumaihuynh91@gmail.com>
2018-09-23 17:35:19 -0500

    Create Chat Rooms: Completed

--- src/components/RoomList/RoomList.js
+++ src/components/RoomList/RoomList.js
@@ -1,37 +1,109 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './RoomList.css';
 
 
-class RoomList extends Component{
-  constructor(props){
+class RoomList extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      value: "",
+      openedForm: false,
+      isPageLoaded: false
     }
-  this.roomsRef = this.props.roomListFirebase.database().ref(); //firebase reference
+
+    this.roomsRef = this.props.roomListFirebase.database().ref('rooms'); //firebase reference
   }
 
-
-  componentDidMount(){
-    this.roomsRef.on('child_added', snapshot => {
+  handleDownloadedData = (snapshot) => {
+    {
+      console.log("data downloaded from firebase");
       const room = snapshot.val();
       room.key = snapshot.key;
-      this.setState({rooms: this.state.rooms.concat(room) });
-    });
+      this.setState({
+        isPageLoaded: true,
+        rooms: this.state.rooms.concat(room)
+      });
+    }
+  }
+  //subscribe, asynchronous
+  componentDidMount() {
+    this.roomsRef.on('child_added', this.handleDownloadedData);
+    console.log("child_added subscibed");
   }
 
-  render(){
-    return(
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-      <div className="sidenav"> 
-        <h1>Bloc Chat</h1>
-        {this.state.rooms.map((room, index) => {
-          return (
-            <a key={index}>{room.name}</a>
-                 )}
-                )}
-      </div> 
-    ) }
+  handleSubmit(e) {
+    e.preventDefault();
+    if(this.state.value!==""){
+      this.roomsRef.push({
+        name: this.state.value
+      })
     }
+    this.setState({ value: "" })
+  }
+
+  handleCancel() {
+    this.setState({
+      openedForm: false,
+      value: ""
+    })
+  }
+
+  createRoom() {
+    this.setState({ openedForm: true })
+  }
+
+
+  checkPageLoaded() {
+    if (this.state.isPageLoaded === true) {
+      return (
+        <div className="sidenav">
+          <h1>Bloc Chat</h1>
+          <button onClick={() => this.createRoom()} >Create Room</button>
+          {this.state.rooms.map((room) => {
+            return (
+              <a key={room.key}>{room.name}</a>
+            )
+          }
+          )}
+        </div>
+      )
+    }
+  }
+
+  renderAddRoomForm() {
+    if (this.state.openedForm === true) {
+      return (
+        <div className="card card-container">
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <div className="form-title">Add A New Room</div>
+
+          <input className="input" type="text" value={this.state.value} onChange={(e) => this.handleChange(e)} />
+
+          <div>
+            <input type="submit" value="Submit" />
+            <button onClick={() => this.handleCancel()}>Cancel</button>
+          </div>
+        </form>
+        </div>
+      )
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.checkPageLoaded()}
+        {this.renderAddRoomForm()}
+      </div>
+
+    )
+  }
+}
 
 export default RoomList;
